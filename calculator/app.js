c5d8c51850af69f8f2e25ca0ec4c45478a0bc72f
Badr Moslih <moslihbadr2@gmail.com>
2023-04-11 01:46:39 +0000

    handle some errors

--- calculator/app.js
+++ calculator/app.js
@@ -1,32 +1,47 @@
-const screen = document.querySelector('#screen');
-const buttons = document.querySelectorAll('button');
+    const screen = document.querySelector('#screen');
+    const buttons = document.querySelectorAll('button');
 
-buttons.forEach(function(item) {
-    item.onclick = function() {
-        if (item.id == 'clear') {
-            screen.value = '';
-        } else if (item.id == 'delete') {
-            let string = screen.value.toString();
-            screen.value = string.slice(0, string.length - 1);
-        } else if (screen.value != '' && item.id == 'equal') {
-            screen.value = eval(screen.value);
-        } else if (screen.value == '' && item.id == 'equal') {
-            screen.value = 'Empty!';
-            setTimeout(function() {
+    buttons.forEach(function(item) {
+        item.onclick = function() {
+            if (item.id == 'clear') {
                 screen.value = '';
-            }, 2000);
-        } else {
-            screen.value += item.value;
-        }
-    };    
-});
-screen.value = '';
-screen.disabled = true;
+            } else if (item.id == 'delete') {
+                let string = screen.value.toString();
+                screen.value = string.slice(0, string.length - 1);
+            } else if (screen.value != '' && item.id == 'equal') {
+                let result;
+                try {
+                    result = eval(screen.value);
+                    if (isNaN(result)) {
+                        throw new Error('Error, try again');
+                    }
+                    if (!isFinite(result)) {
+                        throw new Error('impossible to divide by zero');
+                    }
+                    screen.value = result;
+                } catch (error) {
+                    screen.value = error.message;
+                    setTimeout(function() {
+                        screen.value = '';
+                    }, 2000);
+                }
+            } else if (screen.value == '' && item.id == 'equal') {
+                screen.value = 'Empty!';
+                setTimeout(function() {
+                    screen.value = '';
+                }, 2000);
+            } else {
+                screen.value += item.value;
+            }
+        };    
+    });
+    screen.value = '';
+    screen.disabled = true;
 
 
-let toggleBtn = document.querySelector('.btn');
-let calc = document.querySelector('.calculator');
-toggleBtn.addEventListener('click', function(){
-    toggleBtn.classList.toggle('dark');
-    calc.classList.toggle('dark');
-})
+    let toggleBtn = document.querySelector('.btn');
+    let calc = document.querySelector('.calculator');
+    toggleBtn.addEventListener('click', function(){
+        toggleBtn.classList.toggle('dark');
+        calc.classList.toggle('dark');
+    });
